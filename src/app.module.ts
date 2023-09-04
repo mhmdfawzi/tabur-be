67450865dda595d34330693fcde7f18bd6da0340
Mohamed Fawzy <pl.mhmdfawzi@gmail.com>
2023-09-04 21:59:39 +0300

    -5-

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,33 +6,27 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './modules/auth/auth.module';
 import { RoleModule } from './modules/role/role.module';
 import { CategoryModule } from './modules/category/category.module';
-
-import config from 'ormconfig';
 import { ProviderModule } from './modules/provider/provider.module';
 import { AutomapperModule } from '@automapper/nestjs';
 import { classes } from '@automapper/classes';
 import { ConfigModule } from '@nestjs/config';
-import { PostgresDBConfigService } from './PostgresDBConfigService';
+import { PostgresDBConfigService } from 'src/postgresDBConfig.Service';
 @Module({
   imports: [
     UserModule,
     TypeOrmModule.forRootAsync({
       useClass: PostgresDBConfigService,
       imports: [ConfigModule],
+      inject: [PostgresDBConfigService],
     }),
     AuthModule,
     RoleModule,
     CategoryModule,
     ProviderModule,
     AutomapperModule.forRoot({ strategyInitializer: classes() }),
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
   ],
   controllers: [AppController],
-
-  providers: [AppService],
+  providers: [AppService, PostgresDBConfigService],
 })
-export class AppModule {
-  constructor() {
-    console.log('config', config);
-  }
-}
+export class AppModule {}
